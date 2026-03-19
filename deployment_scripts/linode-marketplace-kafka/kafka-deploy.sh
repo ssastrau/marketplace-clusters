e7669b6902ecd5e3cdbd0465dc243b268bf26116
ssastrau <ssastrau@akamai.com>
2026-03-19 13:13:02 +0100

    Moving Kafka app

---
+++ deployment_scripts/linode-marketplace-kafka/kafka-deploy.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+
+# enable logging
+exec > >(tee /dev/ttyS0 /var/log/stackscript.log) 2>&1
+
+# modes
+#DEBUG="NO"
+if [[ -n ${DEBUG} ]]; then
+	if [ "${DEBUG}" == "NO" ]; then
+		trap "cleanup $? $LINENO" EXIT
+	fi
+else
+	trap "cleanup $? $LINENO" EXIT
+fi
+
+if [ "${MODE}" == "staging" ]; then
+	trap "provision_failed $? $LINENO" ERR
+else
+	set -e
+fi
+
+## deployment variables
+# <UDF name="kafka_version" label="Kafka version" default="3.8.0" oneof="3.8.0" />
+# <UDF name="token_password" label="Your Linode API token" />
+# <UDF name="sudo_username" label="The limited account user" default='admin'>
+# <UDF name="client_count" label="Number of clients connecting to Kafka">
+# <UDF name="cluster_size" label="Kafka cluster size" oneOf="3,5,7">
+# <UDF name="clusterheader" label="Cluster Settings" default="Yes" header="Yes">
+# <UDF name="add_ssh_keys" label="Add Account SSH Keys to All Nodes?" oneof="yes,no"  default="yes" />
+
+# ssl variables
+# <UDF name="sslheader" label="SSL Information" header="Yes" default="Yes" required="Yes">
+# <UDF name="country_name" label="Details for self-signed SSL certificates: Country or Region" oneof="AD,AE,AF,AG,AI,AL,AM,AO,AQ,AR,AS,AT,AU,AW,AX,AZ,BA,BB,BD,BE,BF,BG,BH,BI,BJ,BL,BM,BN,BO,BQ,BR,BS,BT,BV,BW,BY,BZ,CA,CC,CD,CF,CG,CH,CI,CK,CL,CM,CN,CO,CR,CU,CV,CW,CX,CY,CZ,DE,DJ,DK,DM,DO,DZ,EC,EE,EG,EH,ER,ES,ET,FI,FJ,FK,FM,FO,FR,GA,GB,GD,GE,GF,GG,GH,GI,GL,GM,GN,GP,GQ,GR,GS,GT,GU,GW,GY,HK,HM,HN,HR,HT,HU,ID,IE,IL,IM,IN,IO,IQ,IR,IS,IT,JE,JM,JO,JP,KE,KG,KH,KI,KM,KN,KP,KR,KW,KY,KZ,LA,LB,LC,LI,LK,LR,LS,LT,LU,LV,LY,MA,MC,MD,ME,MF,MG,MH,MK,ML,MM,MN,MO,MP,MQ,MR,MS,MT,MU,MV,MW,MX,MY,MZ,NA,NC,NE,NF,NG,NI,NL,NO,NP,NR,NU,NZ,OM,PA,PE,PF,PG,PH,PK,PL,PM,PN,PR,PS,PT,PW,PY,QA,RE,RO,RS,RU,RW,SA,SB,SC,SD,SE,SG,SH,SI,SJ,SK,SL,SM,SN,SO,SR,SS,ST,SV,SX,SY,SZ,TC,TD,TF,TG,TH,TJ,TK,TL,TM,TN,TO,TR,TT,TV,TW,TZ,UA,UG,UM,US,UY,UZ,VA,VC,VE,VG,VI,VN,VU,WF,WS,YE,YT,ZA,ZM,ZW" />
+# <UDF name="state_or_province_name" label="State or Province" example="Example: Pennsylvania" />
+# <UDF name="locality_name" label="Locality" example="Example: Philadelphia" />
+# <UDF name="organization_name" label="Organization" example="Example: Akamai Technologies" />
+# <UDF name="email_address" label="Email Address" example="Example: webmaster@example.com" />
+# <UDF name="ca_common_name" label="CA Common Name" example="Example: Kafka RootCA" />
+
+if [[ -n ${GH_USER} && -n ${BRANCH} ]]; then
+	echo "[info] git user and branch set.."
+	export GIT_REPO="https://github.com/${GH_USER}/marketplace-clusters.git"
+else
+	export GH_USER="akamai-compute-marketplace"
+	export BRANCH="main"
+	export GIT_REPO="https://github.com/${GH_USER}/marketplace-clusters.git"
+fi
+
+export WORK_DIR="/tmp/marketplace-clusters"
+export MARKETPLACE_APP="apps/linode-marketplace-kafka"
+
+if [ -z "${UUID}" ]; then
+	export UUID=$(uuidgen | awk -F - '{print $1}')
+fi
+
+function provision_failed {
+	echo "[info] Provision failed. Sending status.."
+
+	apt install jq -y
+
+	local token=($(curl -ks -X POST ${KC_SERVER} \
+		-H "Content-Type: application/json" \
+		-d "{ \"username\":\"${KC_USERNAME}\", \"password\":\"${KC_PASSWORD}\" }" | jq -r .token))
+
+	curl -sk -X POST ${DATA_ENDPOINT} \
+		-H "Authorization: ${token}" \
+		-H "Content-Type: application/json" \
+		-d "{ \"app_label\":\"${APP_LABEL}\", \"status\":\"provision_failed\", \"branch\": \"${BRANCH}\", \
+        \"gituser\": \"${GH_USER}\", \"runjob\": \"${RUNJOB}\", \"image\":\"${IMAGE}\", \
+        \"type\":\"${TYPE}\", \"region\":\"${REGION}\", \"instance_env\":\"${INSTANCE_ENV}\" }"
+
+	exit $?
+}
+
+function cleanup {
+	if [ "$?" != "0" ]; then
+		echo "PLAYBOOK FAILED. See /var/log/stackscript.log for details."
+		echo "[info] Running Destroy playbook"
+		destroy
+	fi
+
+	if [[ -f "${HOME}/.ssh/id_ansible_ed25519" || -f "${HOME}/.ssh/id_ansible_ed25519.pub" ]]; then
+		echo "[info] Removing provisioner keys.."
+		rm -f "${HOME}/.ssh/id_ansible_ed25519" "${HOME}/.ssh/id_ansible_ed25519.pub"
+	fi
+
+	if [ -d "${WORK_DIR}" ]; then
+		echo "[info] Cleanup - Removing ${WORK_DIR}"
+		rm -rf ${WORK_DIR}
+	fi
+}
+
+function add_privateip {
+	echo "[info] Adding instance private IP"
+	curl -H "Content-Type: application/json" \
+		-H "Authorization: Bearer ${TOKEN_PASSWORD}" \
+		-X POST -d '{
+        "type": "ipv4",
+        "public": false
+      }' \
+		https://api.linode.com/v4/linode/instances/${LINODE_ID}/ips
+}
+
+function get_privateip {
+	curl -s -H "Content-Type: application/json" \
+		-H "Authorization: Bearer ${TOKEN_PASSWORD}" \
+		https://api.linode.com/v4/linode/instances/${LINODE_ID}/ips |
+		jq -r '.ipv4.private[].address'
+}
+
+function configure_privateip {
+	LINODE_IP=$(get_privateip)
+	if [ ! -z "${LINODE_IP}" ]; then
+		echo "[info] Linode private IP present"
+	else
+		echo "[warn] No private IP found. Adding.."
+		add_privateip
+		LINODE_IP=$(get_privateip)
+		ip addr add ${LINODE_IP}/17 dev eth0 label eth0:1
+	fi
+}
+
+function rename_provisioner {
+	echo "[info] renaming the provisioner"
+	curl -s -H "Content-Type: application/json" \
+		-H "Authorization: Bearer ${TOKEN_PASSWORD}" \
+		-X PUT -d "{
+        \"label\": \"kafka-instance-1-${UUID}\"
+      }" \
+		https://api.linode.com/v4/linode/instances/${LINODE_ID}
+}
+
+readonly TEMP_ROOT_PASS=$(openssl rand -base64 32)
+readonly group_vars="${WORK_DIR}/${MARKETPLACE_APP}/group_vars/kafka/vars"
+
+function destroy {
+	cd ${WORK_DIR}/${MARKETPLACE_APP}
+	source env/bin/activate
+	echo "[info] Destroying cluster nodes except provisioner..."
+	ansible-playbook -v destroy.yml
+}
+
+function provisioner_sshkey {
+	echo "[info] Creating provisioner SSH keys..."
+	ssh-keygen -o -a 100 -t ed25519 -C "provisioner" -f "${HOME}/.ssh/id_ansible_ed25519" -q -N "" <<<y >/dev/null
+	export PROVISIONER_SSH_PUB_KEY=$(cat ${HOME}/.ssh/id_ansible_ed25519.pub)
+	export PROVISIONER_SSH_PRIV_KEY=$(cat ${HOME}/.ssh/id_ansible_ed25519)
+	export SSH_KEY_PATH="${HOME}/.ssh/id_ansible_ed25519"
+	chmod 700 ${HOME}/.ssh
+	chmod 600 ${SSH_KEY_PATH}
+	eval $(ssh-agent)
+	ssh-add ${SSH_KEY_PATH}
+	echo -e "\nprivate_key_file = ${SSH_KEY_PATH}" >>${WORK_DIR}/${MARKETPLACE_APP}/ansible.cfg
+}
+
+function provisioner_vars {
+  local LINODE_PARAMS=($(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .type,.region,.image,.disk_encryption))
+  local TAGS=$(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .tags)
+	sed 's/  //g' <<EOF >${group_vars}
+  # user vars
+  token: ${TOKEN_PASSWORD}
+  uuid: ${UUID}
+  ssh_keys: ${PROVISIONER_SSH_PUB_KEY}
+  type: ${LINODE_PARAMS[1]}
+  region: ${LINODE_PARAMS[2]}
+  image: ${LINODE_PARAMS[3]}
+  linode_tags: ${LINODE_TAGS}
+  root_pass: ${TEMP_ROOT_PASS}
+  kafka_version: ${KAFKA_VERSION}
+  cluster_size: ${CLUSTER_SIZE}
+  controller_count: 3
+  client_count: ${CLIENT_COUNT}
+  add_ssh_keys: '${ADD_SSH_KEYS}'
+EOF
+}
+
+# UDF SETUP
+
+function udf {
+	sed 's/  //g' <<EOF >>${group_vars}
+  # sudo username
+  sudo_username: ${SUDO_USERNAME}
+  country_name: ${COUNTRY_NAME}
+  state_or_province_name: ${STATE_OR_PROVINCE_NAME}
+  locality_name: ${LOCALITY_NAME}
+  organization_name: ${ORGANIZATION_NAME}
+  email_address: ${EMAIL_ADDRESS}
+  ca_common_name: ${CA_COMMON_NAME}
+EOF
+
+	if [[ -n ${DOMAIN} ]]; then
+		echo "[info] domain: ${DOMAIN}"
+		echo "domain: ${DOMAIN}" >>${group_vars}
+	else
+		local default_dns
+		default_dns="$(hostname -I | awk '{print $1}' | tr '.' '-' | awk '{print $1 ".ip.linodeusercontent.com"}')"
+		echo "[info] subdomain not set, using default_dns: ${default_dns}"
+		echo "default_dns: ${default_dns}" >>${group_vars}
+	fi
+
+	if [[ -n ${SUBDOMAIN} ]]; then
+		echo "[info] subdomain: ${SUBDOMAIN}"
+		echo "subdomain: ${SUBDOMAIN}" >>${group_vars}
+	else
+		echo "[info] subdomain not set, defaulting to: www"
+		echo "subdomain: www" >>${group_vars}
+	fi
+
+	if [[ -n ${DEBUG} ]]; then
+		echo "[info] debug ${DEBUG} passed"
+		echo "debug: ${DEBUG}" >>${group_vars}
+	fi
+
+	if [ "${ADD_SSH_KEYS}" == "yes" ]; then
+		echo "[info] Adding account SSH keys to authorized_keys..."
+		curl -sH "Content-Type: application/json" \
+			-H "Authorization: Bearer ${TOKEN_PASSWORD}" \
+			https://api.linode.com/v4/profile/sshkeys | jq -r .data[].ssh_key >> /root/.ssh/authorized_keys
+	fi
+
+	# staging or production mode (ci)
+	if [[ "${MODE}" == "staging" ]]; then
+		echo "[info] running in staging mode..."
+		echo "mode: ${MODE}" >>${group_vars}
+	else
+		echo "[info] running in production mode..."
+		echo "mode: production" >>${group_vars}
+	fi
+}
+
+function installation_complete {
+	echo "Installation Complete!"
+}
+
+function run {
+	# install dependencies
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update && apt-get upgrade -y
+	apt-get install -y jq git python3 python3-pip python3-venv
+
+	# rename provisioner and configure private IP
+	rename_provisioner
+	configure_privateip
+
+	# clone repo and set up Ansible environment
+	echo "[info] Cloning ${BRANCH} branch from ${GIT_REPO}..."
+	git -C /tmp clone -b ${BRANCH} ${GIT_REPO}
+	cd ${WORK_DIR}/${MARKETPLACE_APP}
+	python3 -m venv env
+	source env/bin/activate
+	pip install pip --upgrade
+	pip install -r requirements.txt
+	ansible-galaxy install -r collections.yml
+
+	# populate group_vars
+	provisioner_sshkey
+	provisioner_vars
+	udf
+
+	# run playbooks
+	ansible-playbook -v provision.yml && ansible-playbook -v -i hosts site.yml
+}
+
+# main
+run
+installation_complete
